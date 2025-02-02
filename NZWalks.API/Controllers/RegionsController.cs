36d9c7bd3e43de460b5b7d996d4f30092ca1e848
pranavn21 <pranavnair65@gmail.com>
2025-02-01 20:27:03 -0600

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https:images.pexel.com"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https:images.pexel.com"
+
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
